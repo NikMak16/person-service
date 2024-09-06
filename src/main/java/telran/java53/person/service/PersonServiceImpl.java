35f8ee5ad0d123eaf9ca76106673468b1e350637
Nikita <makofthesky@gmail.com>
2024-09-06 21:21:14 +0300

    Added Child and Employee Classes

--- src/main/java/telran/java53/person/service/PersonServiceImpl.java
+++ src/main/java/telran/java53/person/service/PersonServiceImpl.java
@@ -3,20 +3,27 @@ package telran.java53.person.service;
 import java.time.LocalDate;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ch.qos.logback.classic.joran.sanity.IfNestedWithinSecondPhaseElementSC;
 import lombok.RequiredArgsConstructor;
 import telran.java53.person.dao.PersonRepository;
 import telran.java53.person.dto.AddressDto;
+import telran.java53.person.dto.ChildDto;
 import telran.java53.person.dto.CityPopulationDto;
+import telran.java53.person.dto.EmployeeDto;
 import telran.java53.person.dto.PersonDto;
 import telran.java53.person.dto.exceptions.PersonNotFoundException;
 import telran.java53.person.model.Address;
+import telran.java53.person.model.Child;
+import telran.java53.person.model.Employee;
 import telran.java53.person.model.Person;
 @Service
 @RequiredArgsConstructor
-public class PersonServiceImpl implements PersonService {
+public class PersonServiceImpl implements PersonService, CommandLineRunner {
 	
 	final PersonRepository personRepository;
 	final ModelMapper modelMapper;
@@ -26,6 +33,14 @@ public class PersonServiceImpl implements PersonService {
 		if(personRepository.existsById(personDto.getId())) {
 			return false;
 		}
+		if(personDto instanceof ChildDto) {
+			personRepository.save(modelMapper.map(personDto, Child.class));
+			return true;
+		}
+		if(personDto instanceof EmployeeDto) {
+			personRepository.save(modelMapper.map(personDto, Employee.class));
+			return true;
+		}
 		personRepository.save(modelMapper.map(personDto, Person.class));
 		return true;
 	}
@@ -33,7 +48,12 @@ public class PersonServiceImpl implements PersonService {
 	@Override
 	public PersonDto findPersonById(Integer id) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
-		
+		if (person instanceof Child) {
+			return modelMapper.map(person, ChildDto.class);
+		}
+		if (person instanceof Employee) {
+			return modelMapper.map(person, EmployeeDto.class);
+		}
 		return modelMapper.map(person, PersonDto.class);
 	}
 	@Transactional
@@ -48,7 +68,6 @@ public class PersonServiceImpl implements PersonService {
 	public PersonDto updatePersonName(Integer id, String name) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 		person.setName(name);
-//		personRepository.save(person);
 		return modelMapper.map(person, PersonDto.class);
 	}
 	@Transactional
@@ -56,7 +75,6 @@ public class PersonServiceImpl implements PersonService {
 	public PersonDto updatePersonAddress(Integer id, AddressDto addressDto) {
 		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 		person.setAddress(modelMapper.map(addressDto, Address.class));
-//		personRepository.save(person);
 		return modelMapper.map(person, PersonDto.class);
 	}
 
@@ -86,14 +104,29 @@ public class PersonServiceImpl implements PersonService {
 				.toArray(PersonDto[]::new);
 
 	}
-//	@Transactional(readOnly = true)
+
 	@Override
 	public Iterable<CityPopulationDto> getCitiesPopulation() {
-//		return personRepository.findPopulationInEachCity()
-//			.map(a -> new CityPopulationDto(a[1].toString(),  Integer.parseInt(a[0].toString())))
-//			.toList();
-		
+
 		return personRepository.getCityPopulation();
 	}
+@Transactional
+@Override
+public void run(String... args) throws Exception {
+	if(personRepository.count() == 0) {
+		Person person = new Person(1000, "John", LocalDate.of(1985,3,11),
+				new Address("Tel Aviv", "Ben Gvirol", 81));
+		
+		Child child = new Child(2000, "Mosche", LocalDate.of(2019, 7, 5),
+				new Address("Ashkelon", "Bar Kohva", 21), "Shalom");
+		Employee employee = new Employee(3000, "Sarah", LocalDate.of(1995,11,23), 
+				new Address("Rehovot", "Herzl", 7), "Motorola", 20_000);
+		
+		personRepository.save(person);
+		personRepository.save(child);
+		personRepository.save(employee);
+	}
+	
+}
 
 }
